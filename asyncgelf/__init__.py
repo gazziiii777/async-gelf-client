7c25fa5676581712ea5e44ad7d8c3f4afe2cce24
gazziiii777 <gazziiii777@gmail.com>
2026-01-13 16:15:24 +0300

    Bump version to 0.2.0

--- asyncgelf/__init__.py
+++ asyncgelf/__init__.py
@@ -3,7 +3,7 @@
 from .client import AsyncGelfClient
 from .message import GelfMessage, create_gelf_message, convert_python_log_level
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "AsyncGelfClient",
     "GelfMessage",
